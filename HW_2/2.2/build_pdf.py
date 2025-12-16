d66398557b12102ef866d40ce011fefc9fec7f4d
kara-victor <igrovoe2003@gmail.com>
2025-12-17 01:30:03 +0300

    Update build_pdf.py

--- HW_2/2.2/build_pdf.py
+++ HW_2/2.2/build_pdf.py
@@ -1,4 +1,20 @@
+import subprocess
 from latex_generator import generate_table, generate_pic, generate_document
+
+
+def build_pdf(tex_filename: str):
+    try:
+        subprocess.run(
+            ["pdflatex", tex_filename],
+            check=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+    except subprocess.CalledProcessError as e:
+        print("Ошибка при генерации PDF")
+        print(e.stderr.decode("utf-8"))
+        raise
+
 
 data = [
     ["A", "B", "C", "D"],
@@ -8,9 +24,11 @@ data = [
 
 table = generate_table(data)
 pic = generate_pic("sad.jpg")
-
 latex = generate_document(table, pic)
 
-with open("result.tex", "w", encoding="utf-8") as f:
+tex_file = "result.tex"
+
+with open(tex_file, "w", encoding="utf-8") as f:
     f.write(latex)
 
+build_pdf(tex_file)
